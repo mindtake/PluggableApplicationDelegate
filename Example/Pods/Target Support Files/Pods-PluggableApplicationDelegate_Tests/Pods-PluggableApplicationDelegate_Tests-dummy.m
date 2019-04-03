82bf6acb2e0b21fa82944d1cb3fc16642e551b11
BiNo <dominik.weidner@bitsfabrik.com>
2019-04-03 14:15:55 +0200

    remove example

--- Example/Pods/Target Support Files/Pods-PluggableApplicationDelegate_Tests/Pods-PluggableApplicationDelegate_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PluggableApplicationDelegate_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PluggableApplicationDelegate_Tests
-@end
